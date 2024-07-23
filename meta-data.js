20b6d337f696687c7498eab10fad27b6a9776a63
Harsh <harsh.gupta@cognustechnology.com>
2024-07-23 10:56:51 +0530

    mf home page metya added

--- meta-data.js
+++ meta-data.js
@@ -3,8 +3,6 @@ const axios = require("axios");
 const router = express.Router();
 const cheerio = require("cheerio");
 
-const domain = "https://node.onlineinfotech.net";
-
 const metadata = {
   "/": {
     title: "Explore Recents Ipo, Mutual Funds, and Financial Tools | IpoTech ",
@@ -20,28 +18,6 @@ const metadata = {
       "ipos this week",
       "upcoming ipos 2024",
       "best ipo stocks",
-    ],
-    canonical: domain,
-    og: {
-      title:
-        "Explore Recents Ipo, Mutual Funds, and Financial Tools | IpoTech ",
-      description:
-        "Looking for stocks, mutual funds, IPOs, and calculators? IpoTech provides the data and insights you need to invest wisely. Start exploring now.",
-      url: domain,
-      type: "Ipo",
-      image: "og:image",
-    },
-    twitter: {
-      card: "",
-      title:
-        "Explore Recents Ipo, Mutual Funds, and Financial Tools | IpoTech ",
-      description:
-        "Looking for stocks, mutual funds, IPOs, and calculators? IpoTech provides the data and insights you need to invest wisely. Start exploring now.",
-      image: "og:image",
-    },
-    additionalMetaTags: [
-      { name: "author", content: "IpoTech" },
-      { name: "robots", content: "index, follow" },
     ],
   },
 
@@ -60,26 +36,6 @@ const metadata = {
       "upcoming ipos 2024",
       "best ipo stocks",
     ],
-    canonical: `${domain}/ipo`,
-    og: {
-      title: "Discover Mainboard and Upcoming Ipos | IpoTech",
-      description:
-        "Explore upcoming IPOs and Mainboard listings on IpoTech. Stay ahead in the market with the latest IPO news and updates. Discover opportunities now!",
-      url: `${domain}/ipo`,
-      type: "Ipo",
-      image: "og:image",
-    },
-    twitter: {
-      card: "",
-      title: "Discover Mainboard and Upcoming Ipos | IpoTech",
-      description:
-        "Explore upcoming IPOs and Mainboard listings on IpoTech. Stay ahead in the market with the latest IPO news and updates. Discover opportunities now!",
-      image: "og:image",
-    },
-    additionalMetaTags: [
-      { name: "author", content: "IpoTech" },
-      { name: "robots", content: "index, follow" },
-    ],
   },
 
   "/ipo/upcomingIpo": {
@@ -96,26 +52,6 @@ const metadata = {
       "ipos this week",
       "upcoming ipos 2024",
       "best ipo stocks",
-    ],
-    canonical: `${domain}/ipo/upcomingIpo`,
-    og: {
-      title: "Discover Mainboard and Upcoming Ipos | IpoTech",
-      description:
-        "Explore upcoming IPOs and Mainboard listings on IpoTech. Stay ahead in the market with the latest IPO news and updates. Discover opportunities now!",
-      url: `${domain}/ipo/upcomingIpo`,
-      type: "Ipo",
-      image: "og:image",
-    },
-    twitter: {
-      card: "",
-      title: "Discover Mainboard and Upcoming Ipos | IpoTech",
-      description:
-        "Explore upcoming IPOs and Mainboard listings on IpoTech. Stay ahead in the market with the latest IPO news and updates. Discover opportunities now!",
-      image: "og:image",
-    },
-    additionalMetaTags: [
-      { name: "author", content: "IpoTech" },
-      { name: "robots", content: "index, follow" },
     ],
   },
 
@@ -135,26 +71,6 @@ const metadata = {
       "upcoming ipos 2024",
       "best ipo stocks",
     ],
-    canonical: `${domain}/ipo/greyMarketIpo`,
-    og: {
-      title: "Live Grey Market Premium of IPOs | IpoTech",
-      description:
-        "Real-time IPO Grey Market Premiums at your fingertips. Stay informed, stay ahead. Evaluate IPOs with accurate market pricing data today!",
-      url: `${domain}/ipo/greyMarketIpo`,
-      type: "Ipo",
-      image: "og:image",
-    },
-    twitter: {
-      card: "",
-      title: "Live Grey Market Premium of IPOs | IpoTech",
-      description:
-        "Real-time IPO Grey Market Premiums at your fingertips. Stay informed, stay ahead. Evaluate IPOs with accurate market pricing data today!",
-      image: "og:image",
-    },
-    additionalMetaTags: [
-      { name: "author", content: "IpoTech" },
-      { name: "robots", content: "index, follow" },
-    ],
   },
 
   "/ipo/smeMarketIpo": {
@@ -173,26 +89,6 @@ const metadata = {
       "new ipo",
       "nse sme ipo",
     ],
-    canonical: `${domain}/ipo/smeMarketIpo`,
-    og: {
-      title: "Expert Advice on SME IPO Applications | IpoTech",
-      description:
-        "Discover the latest IPO details for Small and Medium-sized Enterprises (SME) on our website, providing valuable insights for investors",
-      url: `${domain}/ipo/smeMarketIpo`,
-      type: "Ipo",
-      image: "og:image",
-    },
-    twitter: {
-      card: "",
-      title: "Expert Advice on SME IPO Applications | IpoTech",
-      description:
-        "Discover the latest IPO details for Small and Medium-sized Enterprises (SME) on our website, providing valuable insights for investors",
-      image: "og:image",
-    },
-    additionalMetaTags: [
-      { name: "author", content: "IpoTech" },
-      { name: "robots", content: "index, follow" },
-    ],
   },
 
   "/ipo/subscriptionStatus": {
@@ -206,55 +102,14 @@ const metadata = {
       "subscribe",
       "pre ipo investing",
     ],
-    canonical: `${domain}/ipo/subscriptionStatus`,
-    og: {
-      title: "IPO Subscription Status - Live from BSE and NSE 2024 | IpoTech",
-      description:
-        "The IPO subscription live status of IPO in 2024. Check how many times Mainboard IPO over-subscribed in QIB, NII and Retail category at BSE, NSE in real-time",
-      url: `${domain}/ipo/subscriptionStatus`,
-      type: "Ipo",
-      image: "og:image",
-    },
-    twitter: {
-      card: "",
-      title: "IPO Subscription Status - Live from BSE and NSE 2024 | IpoTech",
-      description:
-        "The IPO subscription live status of IPO in 2024. Check how many times Mainboard IPO over-subscribed in QIB, NII and Retail category at BSE, NSE in real-time",
-      image: "og:image",
-    },
-    additionalMetaTags: [
-      { name: "author", content: "IpoTech" },
-      { name: "robots", content: "index, follow" },
-    ],
   },
 
   "/ipo/ipoForms": {
-    title: "IPO Forms, Download ASBA IPO Forms, BSE & NSE IPO Forms | IpoTech",
+    title:
+      "IPO Forms, Download ASBA IPO Forms, BSE and NSE IPO Forms | IpoTech",
     description:
       "IPO Forms - Download Mainline IPO and SME IPO forms online. NSE IPO form & BSE IPO form PDF for IPO Application is available for download online",
     keywords: ["ipo forms", "forms", "download forms", "ipo forms download"],
-    canonical: `${domain}/ipo/ipoForms`,
-    og: {
-      title:
-        "IPO Forms, Download ASBA IPO Forms, BSE & NSE IPO Forms | IpoTech",
-      description:
-        "IPO Forms - Download Mainline IPO and SME IPO forms online. NSE IPO form & BSE IPO form PDF for IPO Application is available for download online",
-      url: `${domain}/ipo/ipoForms`,
-      type: "Ipo",
-      image: "og:image",
-    },
-    twitter: {
-      card: "",
-      title:
-        "IPO Forms, Download ASBA IPO Forms, BSE & NSE IPO Forms | IpoTech",
-      description:
-        "IPO Forms - Download Mainline IPO and SME IPO forms online. NSE IPO form & BSE IPO form PDF for IPO Application is available for download online",
-      image: "og:image",
-    },
-    additionalMetaTags: [
-      { name: "author", content: "IpoTech" },
-      { name: "robots", content: "index, follow" },
-    ],
   },
 
   "/ipo/sharesBuyBack": {
@@ -270,27 +125,22 @@ const metadata = {
       "share buyback",
       "buyback",
     ],
-    canonical: `${domain}/ipo/sharesBuyBack`,
-    og: {
-      title:
-        "Upcoming Buyback 2024, Latest Buyback of Shares in India | IpoTech",
-      description:
-        "Share Buyback 2024 - Find the latest share Buyback 2024 offers from the companies with record dates, prices, buyback types and more details ",
-      url: `${domain}/ipo/sharesBuyBack`,
-      type: "Ipo",
-      image: "og:image",
-    },
-    twitter: {
-      card: "",
-      title:
-        "Upcoming Buyback 2024, Latest Buyback of Shares in India | IpoTech",
-      description:
-        "Share Buyback 2024 - Find the latest share Buyback 2024 offers from the companies with record dates, prices, buyback types and more details ",
-      image: "og:image",
-    },
-    additionalMetaTags: [
-      { name: "author", content: "IpoTech" },
-      { name: "robots", content: "index, follow" },
+  },
+
+  "/mutual-funds": {
+    title: "Top Mutual Funds: Smart Investing for Your Future | IpoTech",
+    description:
+      "Explore the best mutual funds to grow your savings. Unlock expert insights for smarter investment choices. Invest in your future now!",
+    keywords: [
+      "mutual funds",
+      "stock vs mutual funds",
+      "mutual fund calculator",
+      "best mutual funds",
+      "Indian stock market",
+      "mutual fund analysis tool",
+      "research tool",
+      "debt funds",
+      "hybrid funds",
     ],
   },
 };
@@ -313,9 +163,8 @@ router.get("/", async (req, res) => {
 
       const summary = await getMfSummary(MfFundCode);
       const meta = summary.data.meta;
-      // Create SEO metadata
       const metaData = {
-        title: `${meta.fullName} | Investment Details & Analysis`,
+        title: `${meta.fullName} | Investment Details and Analysis`,
         description: `Explore the ${meta.fullName} (${meta.plan} Plan) with comprehensive details on performance, risk classification, benchmark index, and investment strategy. Invest wisely with ${meta.amc}.`,
         keywords: [
           meta.fullName,
@@ -326,24 +175,6 @@ router.get("/", async (req, res) => {
           "mutual fund investment",
           "high risk mutual funds",
           "debt and equity funds",
-        ],
-        canonical: `${url}`,
-        og: {
-          title: `${meta.fullName} | Investment Details & Analysis`,
-          description: `Explore the ${meta.fullName} (${meta.plan} Plan) with comprehensive details on performance, risk classification, benchmark index, and investment strategy. Invest wisely with ${meta.amc}.`,
-          url: `${url}`,
-          type: "Mutual Funds",
-          image: "og:image", // Replace with actual image URL
-        },
-        twitter: {
-          card: "",
-          title: `${meta.fullName} | Investment Details & Analysis`,
-          description: `Explore the ${meta.fullName} (${meta.plan} Plan) with comprehensive details on performance, risk classification, benchmark index, and investment strategy. Invest wisely with ${meta.amc}.`,
-          image: "og:image", // Replace with actual image URL
-        },
-        additionalMetaTags: [
-          { name: "author", content: "IpoTech" },
-          { name: "robots", content: "index, follow" },
         ],
       };
       return res.status(200).json(metaData);
@@ -360,24 +191,6 @@ router.get("/", async (req, res) => {
         title: $("title").text().trim(),
         description: $("meta[name='description']").attr("content"),
         keywords: $("meta[name='keywords']").attr("content"),
-        canonical: url,
-        og: {
-          title: $("title").text().trim(),
-          description: $("meta[name='description']").attr("content"),
-          url: url,
-          type: "Ipo",
-          image: $("meta[property='og:image']").attr("content"),
-        },
-        twitter: {
-          card: $("meta[name='twitter:card']").attr("content"),
-          title: $("title").text().trim(),
-          description: $("meta[name='description']").attr("content"),
-          image: $("meta[name='twitter:image']").attr("content"),
-        },
-        additionalMetaTags: [
-          { name: "author", content: "IpoTech" },
-          { name: "robots", content: "index, follow" },
-        ],
       };
       replaceIPOWatchWithIpoTech(metaData);
 
