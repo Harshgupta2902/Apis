134b37accf5d412765b654a26c9281f20de5ea27
Harsh <harsh.gupta@cognustechnology.com>
2024-07-23 14:56:45 +0530

    changes

--- blogs/getblogs.js
+++ blogs/getblogs.js
@@ -9,7 +9,16 @@ router.get("/", async (req, res) => {
       return res.status(404).json({ message: "No blogs found" });
     }
     const blogs = snapshot.docs.map((doc) => ({...doc.data() }));
-    res.status(200).json(blogs);
+    const latestblogs = blogs.length > 0 ? blogs[0] : null;
+    const otherblogs = blogs.slice(1);
+
+    // Construct the response object
+    const response = {
+      latestblogs,
+      otherblogs
+    };
+
+    res.status(200).json(response);
   } catch (error) {
     res.status(500).send(`Error: ${error.message}`);
   }
