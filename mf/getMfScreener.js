15381891f5732ab4f807574802c4d1ea4cb3fba1
Harsh <harsh.gupta@cognustechnology.com>
2024-08-02 10:42:35 +0530

    api chnges

--- mf/getMfScreener.js
+++ mf/getMfScreener.js
@@ -4,8 +4,29 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   const page = req.query.page ?? 0;
+  const category = req.query.category;
+  const risk = req.query.risk;
+
+  let params = [
+    `available_for_investment=true`,
+    `doc_type=scheme`,
+    `page=${page}`,
+    `plan_type=Direct`,
+    `q=`,
+    `size=15`,
+    `sort_by=3`,
+  ];
+
+  if (category) {
+    params.push(`category=${category}`);
+  }
+  if (risk) {
+    params.push(`risk=${risk}`);
+  }
+
+  const url = `https://groww.in/v1/api/search/v1/derived/scheme?${params.join("&")}`;
+
   try {
-    const url = `https://groww.in/v1/api/search/v1/derived/scheme?available_for_investment=true&doc_type=scheme&max_aum=&page=${page}&plan_type=Direct&q=&size=15&sort_by=3`;
     const response = await axios.get(url);
     res.json(response.data);
   } catch (error) {
