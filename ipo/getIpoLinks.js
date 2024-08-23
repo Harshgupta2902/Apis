e9104dc45d977b0fd705ce90833a6a97b5362d04
Harsh <harsh.gupta@cognustechnology.com>
2024-08-23 12:19:40 +0530

    ipo links api changes

--- ipo/getIpoLinks.js
+++ ipo/getIpoLinks.js
@@ -4,48 +4,43 @@ const axios = require("axios");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  try {
-    const [ipoResponse, gmpResponse, buybackResponse, smeResponse] =
-      await Promise.all([
-        axios.get("https://apis-iota-five.vercel.app/api/ipo"),
-        axios.get("https://apis-iota-five.vercel.app/api/gmp"),
-        axios.get("https://apis-iota-five.vercel.app/api/buyback"),
-        axios.get("https://apis-iota-five.vercel.app/api/sme"),
-      ]);
+  const [ipoResponse, gmpResponse, buybackResponse, smeResponse] =
+    await Promise.all([
+      axios.get("https://apis-iota-five.vercel.app/api/ipo"),
+      axios.get("https://apis-iota-five.vercel.app/api/gmp"),
+      axios.get("https://apis-iota-five.vercel.app/api/buyback"),
+      axios.get("https://apis-iota-five.vercel.app/api/sme"),
+    ]);
 
-    console.log("IPO Api Hits SuccessFully :");
-    console.log("GMP Api Hits SuccessFully :");
-    console.log("Buyback Api Hits SuccessFully :");
-    console.log("SME Api Hits SuccessFully :");
+  console.log("IPO Api Hits SuccessFully :");
+  console.log("GMP Api Hits SuccessFully :");
+  console.log("Buyback Api Hits SuccessFully :");
+  console.log("SME Api Hits SuccessFully :");
 
-    // Combine the data
-    const combinedData = {
-      upcomingData: Array.isArray(ipoResponse.data?.upcomingIpos)
-        ? ipoResponse.data.upcomingIpos
-        : [],
-      gmp: Array.isArray(gmpResponse.data?.gmp) ? gmpResponse.data.gmp : [],
-      buyback: Array.isArray(buybackResponse.data?.buyback)
-        ? buybackResponse.data.buyback
-        : [],
-      sme: Array.isArray(smeResponse.data?.smeData)
-        ? smeResponse.data.smeData
-        : [],
-    };
+  // Combine the data
+  const combinedData = {
+    upcomingData: Array.isArray(ipoResponse.data?.upcomingIpos)
+      ? ipoResponse.data.upcomingIpos
+      : [],
+    gmp: Array.isArray(gmpResponse.data?.gmp) ? gmpResponse.data.gmp : [],
+    buyback: Array.isArray(buybackResponse.data?.buyback)
+      ? buybackResponse.data.buyback
+      : [],
+    sme: Array.isArray(smeResponse.data?.smeData)
+      ? smeResponse.data.smeData
+      : [],
+  };
 
-    const slugs = [
-      ...combinedData.upcomingData.map((item) => item.slug),
-      ...combinedData.gmp.map((item) => item.slug),
-      ...combinedData.buyback.map((item) => item.slug),
-      ...combinedData.sme.map((item) => item.slug),
-    ].filter(
-      (slug) => slug !== undefined && slug !== null && slug !== "undefined"
-    );
+  const slugs = [
+    ...combinedData.upcomingData.map((item) => item.slug),
+    ...combinedData.gmp.map((item) => item.slug),
+    ...combinedData.buyback.map((item) => item.slug),
+    ...combinedData.sme.map((item) => item.slug),
+  ].filter(
+    (slug) => slug !== undefined && slug !== null && slug !== "undefined"
+  );
 
-    res.json(slugs);
-  } catch (error) {
-    console.error("Error fetching Additional Ipo Data:", error.message);
-    res.status(404).json({ error: "Internal Server Error" });
-  }
+  res.json(slugs);
 });
 
 module.exports = router;
