5ae435b8c612e999e7125cbc73b4a99bc9274f2e
Harsh <harsh.gupta@cognustechnology.com>
2024-09-24 13:45:10 +0530

    api changed

--- ipo/ipo.js
+++ ipo/ipo.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
-const { generateSlugFromUrl } = require("../utils");
+const { generateSlugFromUrl, sortEntriesByDate } = require("../utils");
 
 const router = express.Router();
 
@@ -56,38 +56,5 @@ router.get("/", async (req, res) => {
   }
 });
 
-const monthToNumber = (month) => {
-  const months = {
-    'Jan.': 1,
-    'Feb.': 2,
-    'Mar.': 3,
-    'Apr.': 4,
-    'May.': 5,
-    'Jun.': 6,
-    'Jul.': 7,
-    'Aug.': 8,
-    'Sept.': 9,
-    'Oct.': 10,
-    'Nov.': 11,
-    'Dec.': 12,
-  };
-  return months[month] || 0; // Return 0 for unknown months
-};
-
-const sortEntriesByDate = (entries) => {
-  return entries.sort((a, b) => {
-    const [dayA, monthA] = a.date.split('-')[0].trim().split(' ');
-    const [dayB, monthB] = b.date.split('-')[0].trim().split(' ');
-
-    const monthNumA = monthToNumber(monthA);
-    const monthNumB = monthToNumber(monthB);
-
-    if (monthNumA !== monthNumB) {
-      return monthNumB - monthNumA; // Sort by month descending
-    } else {
-      return parseInt(dayB) - parseInt(dayA); // Sort by day descending
-    }
-  });
-};
 
 module.exports = router;
--- utils.js
+++ utils.js
@@ -8,6 +8,45 @@ function generateSlugFromUrl(urlString) {
   return lastPart;
 }
 
+
+
+const monthToNumber = (month) => {
+  const months = {
+    'Jan.': 1,
+    'Feb.': 2,
+    'Mar.': 3,
+    'Apr.': 4,
+    'May.': 5,
+    'Jun.': 6,
+    'Jul.': 7,
+    'Aug.': 8,
+    'Sept.': 9,
+    'Oct.': 10,
+    'Nov.': 11,
+    'Dec.': 12,
+  };
+  return months[month] || 0; // Return 0 for unknown months
+};
+
+const sortEntriesByDate = (entries) => {
+  return entries.sort((a, b) => {
+    const [dayA, monthA] = a.date.split('-')[0].trim().split(' ');
+    const [dayB, monthB] = b.date.split('-')[0].trim().split(' ');
+
+    const monthNumA = monthToNumber(monthA);
+    const monthNumB = monthToNumber(monthB);
+
+    if (monthNumA !== monthNumB) {
+      return monthNumB - monthNumA; // Sort by month descending
+    } else {
+      return parseInt(dayB) - parseInt(dayA); // Sort by day descending
+    }
+  });
+};
+
+
+
 module.exports = {
   generateSlugFromUrl,
+  sortEntriesByDate
 };
