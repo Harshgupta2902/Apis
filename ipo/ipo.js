d9c07fb0bd5fb0797c21142bd93682fb452cfa3d
Harsh <harsh.gupta@cognustechnology.com>
2024-09-24 12:40:32 +0530

    ipo api changes

--- ipo/ipo.js
+++ ipo/ipo.js
@@ -17,7 +17,7 @@ router.get("/", async (req, res) => {
         const columns = $(row).find("td");
         const company_name = columns.eq(0).text().trim();
         const link = columns.eq(0).find("a").attr("href");
-        const date = columns.eq(1).text().trim();
+        const dateRange = columns.eq(1).text().trim(); // Capture date range
         const type = columns.eq(2).text().trim();
         const size = columns.eq(3).text().trim();
         const priceband = columns.eq(4).text().trim();
@@ -26,12 +26,12 @@ router.get("/", async (req, res) => {
             ? `https://ipowatch.in/${link}`
             : link;
 
-        let slug = generateSlugFromUrl(`${fullLink}`); // Ensure to handle empty link gracefully
+        let slug = generateSlugFromUrl(`${fullLink}`);
         slug = slug === undefined ? null : slug;
 
         return {
           company_name,
-          date,
+          dateRange, // Include dateRange in the returned object
           type,
           size,
           priceband,
@@ -40,7 +40,9 @@ router.get("/", async (req, res) => {
         };
       })
       .get();
-    const upcomingIpos = ipos.filter(
+
+    const data = sortEntriesByDate(ipos);
+    const upcomingIpos = data.filter(
       (ipo) =>
         ipo.link !== "undefined" &&
         ipo.slug !== "undefined" &&
@@ -53,5 +55,39 @@ router.get("/", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+
+const monthToNumber = (month) => {
+  const months = {
+    'Jan.': 1,
+    'Feb.': 2,
+    'Mar.': 3,
+    'Apr.': 4,
+    'May.': 5,
+    'Jun.': 6,
+    'Jul.': 7,
+    'Aug.': 8,
+    'Sept.': 9,
+    'Oct.': 10,
+    'Nov.': 11,
+    'Dec.': 12,
+  };
+  return months[month] || 0; // Return 0 for unknown months
+};
+
+const sortEntriesByDate = (entries) => {
+  return entries.sort((a, b) => {
+    const [dayA, monthA] = a.dateRange.split('-')[0].trim().split(' ');
+    const [dayB, monthB] = b.dateRange.split('-')[0].trim().split(' ');
+
+    const monthNumA = monthToNumber(monthA);
+    const monthNumB = monthToNumber(monthB);
+
+    if (monthNumA !== monthNumB) {
+      return monthNumB - monthNumA; // Sort by month descending
+    } else {
+      return parseInt(dayB) - parseInt(dayA); // Sort by day descending
+    }
+  });
+};
 
 module.exports = router;
