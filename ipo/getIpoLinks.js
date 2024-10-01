c12d7c4cfe59b2dddcd54021760f4b5b0cf58b19
HarshGupta2902 <harsh.ixora@gmail.com>
2024-10-01 12:41:37 +0530

    ipo links chnaged

--- ipo/getIpoLinks.js
+++ ipo/getIpoLinks.js
@@ -4,17 +4,15 @@ const axios = require("axios");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const [ipoResponse, gmpResponse, buybackResponse, smeResponse] =
-    await Promise.all([
-      axios.get("https://apis-iota-five.vercel.app/api/ipo"),
-      axios.get("https://apis-iota-five.vercel.app/api/gmp"),
-      // axios.get("https://apis-iota-five.vercel.app/api/buyback"),
-      axios.get("https://apis-iota-five.vercel.app/api/sme"),
-    ]);
+  const [ipoResponse, gmpResponse, smeResponse] = await Promise.all([
+    axios.get("https://apis-iota-five.vercel.app/api/ipo"),
+    axios.get("https://apis-iota-five.vercel.app/api/gmp"),
+    // axios.get("https://apis-iota-five.vercel.app/api/buyback"),
+    axios.get("https://apis-iota-five.vercel.app/api/sme"),
+  ]);
 
   console.log("IPO Api Hits SuccessFully :");
   console.log("GMP Api Hits SuccessFully :");
-  console.log("Buyback Api Hits SuccessFully :");
   console.log("SME Api Hits SuccessFully :");
 
   // Combine the data
@@ -23,9 +21,9 @@ router.get("/", async (req, res) => {
       ? ipoResponse.data.upcomingIpos
       : [],
     gmp: Array.isArray(gmpResponse.data?.gmp) ? gmpResponse.data.gmp : [],
-    buyback: Array.isArray(buybackResponse.data?.buyback)
-      ? buybackResponse.data.buyback
-      : [],
+    // buyback: Array.isArray(buybackResponse.data?.buyback)
+    //   ? buybackResponse.data.buyback
+    //   : [],
     sme: Array.isArray(smeResponse.data?.smeData)
       ? smeResponse.data.smeData
       : [],
@@ -34,7 +32,6 @@ router.get("/", async (req, res) => {
   const slugs = [
     ...combinedData.upcomingData.map((item) => item.slug),
     ...combinedData.gmp.map((item) => item.slug),
-    ...combinedData.buyback.map((item) => item.slug),
     ...combinedData.sme.map((item) => item.slug),
   ].filter(
     (slug) => slug !== undefined && slug !== null && slug !== "undefined"
